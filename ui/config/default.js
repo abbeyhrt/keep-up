923fff77d3c95d218f1542a56a338fe8386fb73d
Abbey Hart <abbeyhrt@gmail.com>
2018-06-07 14:51:00 -0500

    feat: removed code

--- ui/config/default.js
+++ ui/config/default.js
@@ -5,7 +5,6 @@ const {
   HOST = 'localhost',
   PORT = 3001,
   NODE_ENV = 'development',
-  PROXY_URL = 'http://localhost:3000',
 } = process.env;
 
 module.exports = {
@@ -13,5 +12,4 @@ module.exports = {
   HOST,
   PORT,
   NODE_ENV,
-  PROXY_URL,
 };
