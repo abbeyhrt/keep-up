d771aba1be37a6487611813f126c3c4e2cdd4dc2
Abbey Hart <abbeyhrt@gmail.com>
2018-08-07 11:12:42 -0500

    feat: add graphql and apollo (#26)
    * feat: add graphql and apollo
    
    * fix: change apollolink to '/graphql'

--- ui/src/client/index.js
+++ ui/src/client/index.js
@@ -3,7 +3,7 @@ import './styles/styles.scss';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 //import gql from 'graphql-tag';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import React from 'react';
@@ -11,9 +11,18 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 
+const link = createHttpLink({
+  uri: '/graphql',
+  credentials: 'include',
+  fetchOptions: {
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+  },
+});
+
 const client = new ApolloClient({
-  link: new HttpLink(),
   cache: new InMemoryCache(),
+  link,
 });
 
 const mountNode = document.getElementById('root');
