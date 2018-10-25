078e4087a1501ba0101b77cab226aae74ab6e58e
Josh Black <josh@josh.black>
2018-10-25 09:49:21 -0500

    feat(proxy): update prod config (#39)
    * feat(proxy): update prod config
    
    * chore: update CI config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * chore: update ci config
    
    * fix(proxy): update conf to remove host header
    
    * chore(ci): move steps for deploy to master branch

---
+++ proxy/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$SITE" == "localhost" ]; then
+  # https://letsencrypt.org/docs/certificates-for-localhost/
+  openssl req -x509 -out /etc/ssl/certs/localhost.crt -keyout /etc/ssl/certs/localhost.key \
+    -newkey rsa:2048 -nodes -sha256 \
+    -subj '/CN=localhost' -extensions EXT -config <( \
+    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+fi
