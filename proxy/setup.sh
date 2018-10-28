141466f0eb0f4187777286b34ab44588a762e613
Josh Black <josh@josh.black>
2018-10-28 14:55:48 -0500

    fix(proxy): update local path filename for certs

--- proxy/setup.sh
+++ proxy/setup.sh
@@ -2,7 +2,7 @@
 
 if [ "$SITE" == "localhost" ]; then
   # https://letsencrypt.org/docs/certificates-for-localhost/
-  openssl req -x509 -out /etc/ssl/certs/localhost.crt -keyout /etc/ssl/certs/localhost.key \
+  openssl req -x509 -out '/etc/ssl/certs/keepup.local.crt' -keyout '/etc/ssl/certs/keepup.local.key' \
     -newkey rsa:2048 -nodes -sha256 \
     -subj '/CN=localhost' -extensions EXT -config <( \
     printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
